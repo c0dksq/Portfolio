97b2f6e9a39c133a91b99f608680e820b4a24b6f
c0dksq <165921557+c0dksq@users.noreply.github.com>
2026-09-09 12:16:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ const PROJECTS = [
     title: "Matchday Posters: Man City",
     category: "Social Media",
     year: "2026-27",
-    image: "https://picsum.photos/seed/verano/800/1000",
+    image: "image/matchday26/PALACEAWAYPL.jpg",
     description: "I have Challenged myself to create a poster for every single match Manchester City Play in the 26-27 football season",
     link: "everymatchday.html"
   },
